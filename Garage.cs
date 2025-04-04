8b47f68b7d317dd04648913b72cecf8b1aebf938
Kab-31-16 <Kab-31-16@KAB-31-16-PC>
2025-04-04 12:43:42 +0300

    123

--- Garage.cs
+++ Garage.cs
@@ -8,5 +8,130 @@ namespace prac34
 {
     class Garage
     {
+        public List<Lorry> lorrys = new List<Lorry>();
+        public List<Car> cars = new List<Car>();
+        public List<Bicycle> bicycles = new List<Bicycle>();
+
+        public void AddLorry()
+        {
+            int prise = 0, maxspeed = 0, year = 0, MaxCapacity = 0;
+            Console.Write("Введите максимальную скорость: ");
+            try
+            {
+                maxspeed = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.Write("Введите грузоподъемность: ");
+                try
+                {
+                    MaxCapacity = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    lorrys.Add(new Lorry(prise, maxspeed, year, MaxCapacity));
+                }
+            }
+        }
+
+        public void AddBicycle()
+        {
+            int prise = 0, maxspeed = 0, year = 0, MaxPassengers = 0;
+            Console.WriteLine("Введите цену:");
+            try
+            {
+                prise = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Введите максимальную скорость");
+                try
+                {
+                    maxspeed = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введите год:");
+                    try
+                    {
+                        year = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Введите количество пассажиров:");
+                        try
+                        {
+                            MaxPassengers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            bicycles.Add(new Bicycle(prise, maxspeed, year, MaxPassengers));
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AddCar()
+        {
+            int prise = 0, maxspeed = 0, year = 0, Power = 0;
+            Console.WriteLine("Введите цену:");
+            try
+            {
+                prise = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Введите максимальную скорость");
+                try
+                {
+                    maxspeed = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Введите год:");
+                    try
+                    {
+                        year = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Введите мощность:");
+                        try
+                        {
+                            Power = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            cars.Add(new Car(prise, maxspeed, year, Power));
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GetAllLorry()
+        {
+            foreach (Lorry lorry in lorrys)
+            {
+                Console.WriteLine($"Год: {lorry.Year}\tМаксимальная скорость: {lorry.Maxspeed}\tГрузоподъемность: {lorry.MaxCapacity}");
+            }
+        }
+
+        public void GetAllCar()
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"Год: {car.Year}\tМаксимальная скорость: {car.Maxspeed}\tМощность: {car.Power}");
+            }
+        }
+
+        public void GetAllBicycle()
+        {
+            foreach (Bicycle bicycle in bicycles)
+            {
+                Console.WriteLine($"Год: {bicycle.Year}\tМаксимальная скорость: {bicycle.Maxspeed}\tКоличество пассажиров: {bicycle.MaxPassengers}");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,36 @@ namespace prac34
     {
         static void Main(string[] args)
         {
+            Garage garrage = new Garage();
+            while (true)
+            {
+                int i = 0;
+                Console.WriteLine("ВЫБЕРИТЕ НУЖНОЕ ВАМ ДЕЙСТВИЕ:");
+                Console.WriteLine("1. Добавить в список велосипед");
+                Console.WriteLine("2. Добавить в список грузовик");
+                Console.WriteLine("3. Добавить в список машину");
+                Console.WriteLine("4. Вывести все машины из списка:");
+                Console.WriteLine("5. Вывести все грузовики из списка:");
+                Console.WriteLine("6. Вывести все грузовики из списка:");
+                Console.WriteLine("7. Выход");
+                try
+                {
+                    i = Convert.ToInt32(Console.ReadLine());
+                    switch (i)
+                    {
+                        case 1: garrage.AddBicycle(); break;
+                        case 2: garrage.AddLorry(); break;
+                        case 3: garrage.AddCar(); break;
+                        case 4: Console.WriteLine("Велосипеды:"); garrage.GetAllBicycle(); break;
+                        case 5: Console.WriteLine("Машины:"); garrage.GetAllCar(); break;
+                        case 6: Console.WriteLine("Грузовики:"); garrage.GetAllLorry(); break;
+                        case 7: return;
+                        default: Console.WriteLine(""); break;
+                    }
+                }
+                catch { }
+                Console.Read();
+            }
         }
     }
 }
